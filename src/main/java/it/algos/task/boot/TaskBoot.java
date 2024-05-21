1040c98b6d93b771f18a340a8ac783a54728cc2b
gac <info@algos.it>
2024-05-21 19:02:28 +0200

    - fixRoutes

--- src/main/java/it/algos/task/boot/TaskBoot.java
+++ src/main/java/it/algos/task/boot/TaskBoot.java
@@ -1,15 +1,7 @@
 package it.algos.task.boot;
 
 import it.algos.vbase.backend.boot.*;
-import static it.algos.vbase.backend.boot.BaseCost.*;
-import static it.algos.vbase.backend.boot.BaseVar.*;
-import it.algos.vbase.backend.logic.*;
-import it.algos.vbase.backend.pref.*;
-import it.algos.vbase.backend.wrapper.*;
-import it.algos.vbase.ui.view.*;
 import org.springframework.stereotype.*;
-
-import java.util.*;
 
 /**
  * Project wiki24
@@ -31,44 +23,44 @@ public class TaskBoot extends BaseBoot {
         this.creaTask();
     }
 
-    /**
-     * Aggiunge al menu le @Route (view) standard e specifiche <br>
-     * Questa classe viene invocata PRIMA della chiamata del browser <br>
-     * <p>
-     * Nella sottoclasse che invoca questo metodo, aggiunge le @Route (view) specifiche dell' applicazione <br>
-     * Le @Route vengono aggiunte ad una Lista statica mantenuta in BaseVar <br>
-     * Verranno lette da MainLayout la prima volta che il browser 'chiama' una view <br>
-     * Può essere sovrascritto, invocando PRIMA il metodo della superclasse <br>
-     */
-    @Override
-    protected void fixRoutes() {
-        super.fixRoutes();
-
-        List<Class> listaViewsProject;
-        String message;
-        String viewName;
-        String pathView="it.algos.task";
-
-        if (Pref.usaMenuAutomatici.is()) {
-            listaViewsProject = reflectionService.getSubClazz(CrudView.class,pathView);
-            if (listaViewsProject != null) {
-                for (Class clazz : listaViewsProject) {
-                    if (annotationService.usaMenuAutomatico(clazz)) {
-                        viewClazzListProject.add(clazz);
-                        viewName = clazz.getSimpleName();
-                        viewName = textService.levaCoda(viewName, SUFFIX_VIEW);
-                        nameViewListProject.add(viewName);
-                    }
-                }
-            }
-            else {
-                message = String.format("Non esiste nessuna view/route nel progetto [%s]", projectCurrent);
-                logger.warn(new WrapLog().exception(new Exception(message)));
-            }
-        }
-        else {
-        }
-    }
+//    /**
+//     * Aggiunge al menu le @Route (view) standard e specifiche <br>
+//     * Questa classe viene invocata PRIMA della chiamata del browser <br>
+//     * <p>
+//     * Nella sottoclasse che invoca questo metodo, aggiunge le @Route (view) specifiche dell' applicazione <br>
+//     * Le @Route vengono aggiunte ad una Lista statica mantenuta in BaseVar <br>
+//     * Verranno lette da MainLayout la prima volta che il browser 'chiama' una view <br>
+//     * Può essere sovrascritto, invocando PRIMA il metodo della superclasse <br>
+//     */
+//    @Override
+//    protected void fixRoutes() {
+//        super.fixRoutes();
+//
+//        List<Class> listaViewsProject;
+//        String message;
+//        String viewName;
+//        String pathView="it.algos.task";
+//
+//        if (Pref.usaMenuAutomatici.is()) {
+//            listaViewsProject = reflectionService.getSubClazz(CrudView.class,pathView);
+//            if (listaViewsProject != null) {
+//                for (Class clazz : listaViewsProject) {
+//                    if (annotationService.usaMenuAutomatico(clazz)) {
+////                        viewClazzListProject.add(clazz);
+//                        viewName = clazz.getSimpleName();
+//                        viewName = textService.levaCoda(viewName, SUFFIX_VIEW);
+//                        nameViewListProject.add(viewName);
+//                    }
+//                }
+//            }
+//            else {
+//                message = String.format("Non esiste nessuna view/route nel progetto [%s]", projectCurrent);
+//                logger.warn(new WrapLog().exception(new Exception(message)));
+//            }
+//        }
+//        else {
+//        }
+//    }
 
 
     protected void creaTask() {
