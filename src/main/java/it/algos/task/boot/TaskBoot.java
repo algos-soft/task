bf1ee13bd3f61c53cb6ea63187dc775e5d888776
gac <info@algos.it>
2024-05-22 18:36:12 +0200

    - varie

--- src/main/java/it/algos/task/boot/TaskBoot.java
+++ src/main/java/it/algos/task/boot/TaskBoot.java
@@ -23,44 +23,6 @@ public class TaskBoot extends BaseBoot {
         this.creaTask();
     }
 
-//    /**
-//     * Aggiunge al menu le @Route (view) standard e specifiche <br>
-//     * Questa classe viene invocata PRIMA della chiamata del browser <br>
-//     * <p>
-//     * Nella sottoclasse che invoca questo metodo, aggiunge le @Route (view) specifiche dell' applicazione <br>
-//     * Le @Route vengono aggiunte ad una Lista statica mantenuta in BaseVar <br>
-//     * Verranno lette da MainLayout la prima volta che il browser 'chiama' una view <br>
-//     * Può essere sovrascritto, invocando PRIMA il metodo della superclasse <br>
-//     */
-//    @Override
-//    protected void fixRoutes() {
-//        super.fixRoutes();
-//
-//        List<Class> listaViewsProject;
-//        String message;
-//        String viewName;
-//        String pathView="it.algos.task";
-//
-//        if (Pref.usaMenuAutomatici.is()) {
-//            listaViewsProject = reflectionService.getSubClazz(CrudView.class,pathView);
-//            if (listaViewsProject != null) {
-//                for (Class clazz : listaViewsProject) {
-//                    if (annotationService.usaMenuAutomatico(clazz)) {
-////                        viewClazzListProject.add(clazz);
-//                        viewName = clazz.getSimpleName();
-//                        viewName = textService.levaCoda(viewName, SUFFIX_VIEW);
-//                        nameViewListProject.add(viewName);
-//                    }
-//                }
-//            }
-//            else {
-//                message = String.format("Non esiste nessuna view/route nel progetto [%s]", projectCurrent);
-//                logger.warn(new WrapLog().exception(new Exception(message)));
-//            }
-//        }
-//        else {
-//        }
-//    }
 
 
     protected void creaTask() {
