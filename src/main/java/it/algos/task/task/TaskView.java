338710e09453948a5eeac507e47428ea96c59bbd
gac <info@algos.it>
2024-07-01 16:29:19 +0200

    -Cost

--- src/main/java/it/algos/task/task/TaskView.java
+++ src/main/java/it/algos/task/task/TaskView.java
@@ -2,11 +2,11 @@ package it.algos.task.task;
 
 import com.vaadin.flow.router.Route;
 import it.algos.vbase.backend.annotation.AView;
+import it.algos.vbase.backend.constant.Gruppo;
 import it.algos.vbase.ui.view.CrudView;
 import it.algos.vbase.ui.view.MainLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static it.algos.task.boot.TaskCost.GROUP_TASK;
 
 /**
  * Project base24
@@ -18,7 +18,7 @@ import static it.algos.task.boot.TaskCost.GROUP_TASK;
  * @Route chiamata dal menu generale o dalla barra del browser <br>
  */
 @Route(value = "task", layout = MainLayout.class)
-@AView(menuGroup = GROUP_TASK, menuName = "Task")
+@AView(menuGroup = Gruppo.TASK, menuName = "Task")
 public class TaskView extends CrudView {
 
 
