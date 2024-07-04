66f53de87a7055648613e182b0f636a0fe18b110
gac <info@algos.it>
2024-07-04 07:55:01 +0200

    -hello

---
+++ src/main/java/it/algos/task/HelloWorldView.java
@@ -0,0 +1,20 @@
+package it.algos.task;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import it.algos.vbase.ui.view.MainLayout;
+
+@Route(value = "hello", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Uses(Icon.class)
+public class HelloWorldView extends Composite<VerticalLayout> {
+
+    public HelloWorldView() {
+        getContent().setHeightFull();
+        getContent().setWidthFull();
+    }
+}
