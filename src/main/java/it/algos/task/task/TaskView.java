453de271fa7bff49bea0ffea08593c2f6164dbc7
gac <info@algos.it>
2024-07-15 12:55:46 +0200

    varie

--- src/main/java/it/algos/task/task/TaskView.java
+++ src/main/java/it/algos/task/task/TaskView.java
@@ -1,5 +1,6 @@
 package it.algos.task.task;
 
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.Route;
 import it.algos.vbase.backend.annotation.AView;
 import it.algos.vbase.backend.constant.Gruppo;
@@ -18,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Route chiamata dal menu generale o dalla barra del browser <br>
  */
 @Route(value = "task", layout = MainLayout.class)
-@AView(menuGroup = Gruppo.TASK, menuName = "Task")
+@AView(menuGroup = Gruppo.TASK, menuName = "Task", vaadin = VaadinIcon.STOPWATCH)
 public class TaskView extends CrudView {
 
 
