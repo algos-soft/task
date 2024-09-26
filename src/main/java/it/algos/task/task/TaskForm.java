74cc43ccc993cfa700694ad06a11d4e800d31cd2
gac <info@algos.it>
2024-09-26 22:49:00 +0200

    varie

--- src/main/java/it/algos/task/task/TaskForm.java
+++ src/main/java/it/algos/task/task/TaskForm.java
@@ -2,27 +2,18 @@ package it.algos.task.task;
 
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import it.algos.vbase.backend.entity.AbstractEntity;
-import it.algos.vbase.backend.enumeration.CrudOperation;
-import it.algos.vbase.backend.list.AList;
-import it.algos.vbase.ui.form.CrudForm;
+import it.algos.vbase.backend.form.DefaultForm;
 import org.springframework.context.annotation.Scope;
 
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @SpringComponent
 @Scope(value = SCOPE_PROTOTYPE)
-public class TaskForm extends CrudForm {
+public class TaskForm<T extends AbstractEntity> extends DefaultForm<T> {
 
 
-    //--new entityBean and update existing entityBean
-    public TaskForm(final AList parentAList, AbstractEntity entityBean, CrudOperation operation) {
-        super(parentAList, entityBean, operation);
+    public TaskForm(T bean) {
+        super(bean);
     }
 
-
-    @Override
-    protected void override() {
-        //--qui eventuali regolazioni specifiche delle variabili
-    }
-
-}// end of CrudForm class
+}
